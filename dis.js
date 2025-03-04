4e642cfb17cbd74d2c6c73e8a59cf0fdf2df4052
Kristen <harunakristen@gmail.com>
2025-03-04 13:50:22 +0000

    first commit

---
+++ dis.js
@@ -0,0 +1,16 @@
+let No1 = 0;
+let noTwo = 2;
+
+if (No1 > noTwo) {
+  console.log("number is greater");
+} 
+else if (No1 < noTwo) {
+
+  console.log("num. is lesser");
+} 
+if (No1 === 0 && noTwo === 0) {
+  console.log("neutral");
+}
+
+
+// number is o..no is 0
---
+++ style.js
@@ -0,0 +1,12 @@
+let fixedNum = 0;
+let inputNum = 2;
+
+if (inputNum > fixedNum) {
+  return "Positive NUm";
+} else if (inputNum < fixedNum) {
+  return "Negative number";
+} else if (inputNum === fixedNum) {
+  return"number is neutral";
+}
+
+console.log(inputNum);
